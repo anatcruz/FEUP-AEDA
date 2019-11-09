8a366417e445b922f80ac35d2bb38ab841759e0b
anatcruz <anateresacruz2000@gmail.com>
2019-11-09 15:34:57 +0000

    Added client as a new member

--- Classes/Order.h
+++ Classes/Order.h
@@ -7,12 +7,14 @@
 #include "Date.h"
 #include "Product.h"
 #include "Worker.h"
+#include "Client.h"
 #include "../utils.h"
 
 using namespace std;
 
 class Restaurant;
 class Deliveryperson;
+class Client;
 
 class Order {
 protected:
@@ -21,21 +23,24 @@ protected:
     int orderTime;
     vector<Product> products;
     float orderPrice;
+    Client* client;
 public:
     Order() = default;
-    Order(Restaurant* restaurant, Date date, int orderTime, vector<Product> products, float orderPrice);
+    Order(Restaurant* restaurant, Date date, int orderTime, vector<Product> products, float orderPrice, Client* client);
     //Metodos Set
     void setOrderRestaurant(Restaurant* restaurant);
     void setOrderDate(Date date);
     void setOrderTime(int orderTime);
     void setOrderProducts(vector<Product> products);
     void setOrderPrice(float orderPrice);
+    void setOrderClient(Client* client);
     //Metodos Get
     Restaurant* getRestaurant() const;
     Date getOrderDate() const;
     int getOrderTime() const;
     vector<Product> getOrderProducts() const;
     float getOrderPrice() const;
+    Client* getOrderClient() const;
     //Other Methods
 
 };
