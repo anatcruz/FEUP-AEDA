40b0bcee8a41279dafbbca26d732789a4c7b3bd5
anatcruz <anateresacruz2000@gmail.com>
2019-11-11 21:52:44 +0000

    Created a new class and all the functions associated to it

--- Classes/Time.cpp
+++ Classes/Time.cpp
@@ -5,3 +5,37 @@ Time::Time(int hour, int min, int sec) {
     this->min = min;
     this->sec = sec;
 }
+
+//Metodos Set
+void Time::setHour(int hour) {
+    this->hour = hour;
+}
+
+void Time::setMin(int min) {
+    this->min = min;
+}
+
+void Time::setSec(int sec) {
+    this->sec = sec;
+}
+
+//Metodos Get
+int Time::getHour() const {
+    return hour;
+}
+
+int Time::getMin() const {
+    return min;
+}
+
+int Time::getSec() const {
+    return sec;
+}
+
+//Other Methods
+ostream &operator<<(ostream &out, const Time &time) {
+    out << setfill('0') << right << setw(2) << to_string(time.hour);
+    out << ":" << setfill('0') << setw(2) << to_string(time.min);
+    out << ":" << setfill('0') << setw(2) << to_string(time.sec);
+    return out;
+}
--- Classes/Time.h
+++ Classes/Time.h
@@ -1,14 +1,31 @@
 #ifndef AEDA_UGHEATS_TIME_H
 #define AEDA_UGHEATS_TIME_H
 
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
 
 class Time {
-public:
+private:
     int hour;
     int min;
     int sec;
+public:
     Time() = default;
     Time(int hour, int min, int sec);
+    //Metodos Set
+    void setHour(int hour);
+    void setMin(int min);
+    void setSec(int sec);
+    //Metodos Get
+    int getHour() const;
+    int getMin() const;
+    int getSec() const;
+    //Other Methods
+    friend ostream& operator<<(ostream& out, const Time &time);
 };
 
 
