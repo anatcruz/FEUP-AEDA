db7a88813b48515b10d324dc9ff03867fd802500
anatcruz <anateresacruz2000@gmail.com>
2019-11-11 21:53:20 +0000

    Made some alterations because of the new class Time

--- Classes/Order.h
+++ Classes/Order.h
@@ -8,6 +8,7 @@
 #include "Product.h"
 #include "Worker.h"
 #include "Client.h"
+#include "Time.h"
 #include "../utils.h"
 
 using namespace std;
@@ -15,29 +16,30 @@ using namespace std;
 class Restaurant;
 class Deliveryperson;
 class Client;
+class Time;
 
 class Order {
 protected:
     Restaurant* restaurant;
     Client* client;
     Date date;
-    int orderTime;
+    Time orderTime;
     vector<Product> products;
     float orderPrice;
 public:
     Order() = default;
-    Order(Restaurant* restaurant, Client* client, Date date, int orderTime, vector<Product> products, float orderPrice);
+    Order(Restaurant* restaurant, Client* client, Date date, Time orderTime, vector<Product> products, float orderPrice);
     //Metodos Set
     void setOrderRestaurant(Restaurant* restaurant);
     void setOrderDate(Date date);
-    void setOrderTime(int orderTime);
+    void setOrderTime(Time orderTime);
     void setOrderProducts(vector<Product> products);
     void setOrderPrice(float orderPrice);
     void setOrderClient(Client* client);
     //Metodos Get
     Restaurant* getRestaurant() const;
     Date getOrderDate() const;
-    int getOrderTime() const;
+    Time getOrderTime() const;
     vector<Product> getOrderProducts() const;
     float getOrderPrice() const;
     Client* getOrderClient() const;
@@ -51,22 +53,22 @@ private:
     float deliveryPrice;
     Deliveryperson* deliveryperson;
     bool success;
-    int deliveryTime;
+    Time deliveryTime;
     string notes;
 public:
     Delivery() = default;
-    Delivery(Restaurant* restaurant, Client* client, Date date, int orderTime, vector<Product> products, float orderPrice, float deliveryPrice, Deliveryperson* deliveryperson, bool success, int deliveryTime, string notes);
+    Delivery(Restaurant* restaurant, Client* client, Date date, Time orderTime, vector<Product> products, float orderPrice, float deliveryPrice, Deliveryperson* deliveryperson, bool success, Time deliveryTime, string notes);
     //Metodos Set
     void setDeliveryPrice(float deliveryPrice);
     void setDeliveryPerson(Deliveryperson* deliveryperson);
     void setSuccess(bool success);
-    void setDeliveryTime(int deliveryTime);
+    void setDeliveryTime(Time deliveryTime);
     void setDeliveryNotes(string notes);
     //Metodos Get
     float getDeliveryPrice() const;
     Deliveryperson* getDeliveryPerson() const;
     bool getSuccess() const;
-    int getDeliveryTime() const;
+    Time getDeliveryTime() const;
     string getDeliveryNotes() const;
     //Other Methods
     friend ostream& operator<<(ostream& out, const Delivery &delivery);
