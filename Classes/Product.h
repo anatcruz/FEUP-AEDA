f2bd393654290383f7d9089f5c49d8c6cce98f41
anatcruz <anateresacruz2000@gmail.com>
2019-11-13 16:53:11 +0000

    Documented!

--- Classes/Product.h
+++ Classes/Product.h
@@ -12,17 +12,43 @@ private:
     string cuisine;
     float price;
 public:
+    /** Constructor default.
+*/
     Product() = default;
+    /** Constructor of a Product from the name, cuisine and price.
+*/
     Product(string name, string cuisine, float price);
     //Metodos Set
+    /** Sets the name of a Product.
+       * @param name is the parameter you want the new Product to have.
+*/
     void setProductName(string name);
+    /** Sets the cuisine of a Product.
+       * @param cuisine is the parameter you want the new Product to have.
+*/
     void setCuisine(string cuisine);
+    /** Sets the price of a Product.
+       * @param price is the parameter you want the new Product to have.
+*/
     void setPrice(float price);
     //Metodos Get
+    /**
+       * @return the name.
+*/
     string getProductName() const;
+    /**
+       * @return the cuisine.
+*/
     string getCuisine() const;
+    /**
+       * @return the price.
+*/
     float getPrice() const;
     //Other Methods
+    /** Displays the Product in a nice format.
+      * @param out is the ostream.
+      * @param product is the Product you want to display.
+     */
     friend ostream& operator<<(ostream& out, const Product &product);
 };
 
